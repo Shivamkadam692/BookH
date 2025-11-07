c9edc90b59978166ba00498ca0394965a572bf4d
Shivamkadam692 <shivam.123kadam@gmail.com>
2025-11-07 16:02:13 +0530

    auathentication routes added

--- app.js
+++ app.js
@@ -5,10 +5,21 @@ const path = require("path");
 const methodoverride = require("method-override");
 const ejsmate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
-const  listings = require('./routes/listing.js');
-const reviews = require('./routes/review.js');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user.js');
+
+
+
+
+
+const  listings = require('./routes/listing.js');
+const reviews = require('./routes/review.js');
+const userRoutes = require('./routes/user.js');
+
+
 
 
 const MONGO_URL = 'mongodb://127.0.0.1:27017/BookH';
@@ -49,6 +60,16 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+
+
 app.get("/", (req, res) => {
     res.send("iam root");
 });
@@ -57,6 +78,7 @@ app.get("/", (req, res) => {
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
+    res.locals.currentUser = req.user;
     next();
 });
 
@@ -64,6 +86,7 @@ app.use((req, res, next) => {
 //Routers 
 app.use('/listings', listings);
 app.use('/listings/:id/reviews', reviews);
+app.use('/', userRoutes);
 
 
 
---
+++ routes/user.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const router = express.Router();
+const user = require('../models/user.js');
+const wrapAsync = require("../utils/wrapAsync.js");
+const passport = require("passport");
+
+//Signup routes
+
+router.get('/signup', (req, res) => {
+    res.render('users/signup.ejs');
+});
+
+router.post('/signup', wrapAsync(async (req, res) => {
+
+    try {
+        let { username, email, password } = req.body;
+        const newUser = new user({ email, username });
+        const registeredUser = await user.register(newUser, password);
+        req.login(registeredUser, (err)=> {
+        if(err){
+            return next(err);
+        }
+        req.flash('success', 'Welcome to BookH!');
+        res.redirect('/listings');
+    })
+    } catch (e) {
+        req.flash('error', e.message);
+        res.redirect('/signup');
+    }
+
+}));
+
+
+
+//login routes
+
+router.get('/login', (req, res) => {
+    res.render('users/login.ejs');
+});
+
+router.post('/login', passport.authenticate("local", { failureRedirect: '/login', failureFlash: true }), wrapAsync (async (req, res) => {
+
+
+    req.flash('success', 'Welcome Back!');
+    res.redirect('/listings');
+}));
+
+
+//logout route
+
+router.get('/logout', (req, res,next) => {
+    req.logout((err) => {
+        if (err) {
+
+            return next(err);
+        }
+        req.flash('success', "Your have logged out!");
+        res.redirect('/listings');
+    });
+});
+
+module.exports = router;
